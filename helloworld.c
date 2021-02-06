e5e919c9fb6474319de42ae279ef13ab4041141c
cudnart99 <cudnart99@gmail.com>
2021-02-06 10:37:24 +0700

    commit test and helloworld to repository

---
+++ helloworld.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int a=1,b=2;
+    printf("nhap a : ");
+    scanf("%d",&a);
+    printf("nhap b : ");
+    scanf("%d",&b);
+    printf("tong a va b la : %d",a+b);
+    return 0;
+}
